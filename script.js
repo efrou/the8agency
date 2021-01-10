4bd6f517efebdab83a9ac305362c2f8dbf65184f
Florencia Romero <fmromer@gmail.com>
2021-01-10 19:00:30 -0300

    local storage implementation done

--- script.js
+++ script.js
@@ -1,55 +1,31 @@
-//Get data
-
-let inputName = document.getElementById("name");
-let inputLastName = document.getElementById("last-name");
-let inputEmail = document.getElementById("email");
-let inputCountry = document.getElementById("country");
-let inputPhone = document.getElementById("phone-number");
-let inputJob = document.getElementById("job");
-
-
-let registeredPerson = {
-    name   : inputName.value,
-    last : inputLastName.value,
-    email      : inputEmail.value,
-    country    : inputCountry.value,
-    phone : inputPhone.value,
-    job : inputJob.value
-  };
-
-
-/*
-var registeredPerson = {
-    name: "Hola",
-    number: 60
-};
- 
-*/
-
-
 //Save data in LocalStorage
 
 function save_data(){
-
-let registeredPerson_serialized = JSON.stringify(registeredPerson);
-/*
-function save_data() {
-  localStorage.setItem("Person", registeredPerson_serialized); 
-};
-*/
-
-localStorage.setItem("Person", registeredPerson_serialized); 
-
- //Get data
-let registeredPerson_deserialized = JSON.parse(localStorage.getItem("Person"));
-
-console.log(registeredPerson_deserialized);
-
-/*
-var storedValue = JSON.parse(localStorage.getItem("Person")); 
-
-console.log(storedValue);
-
-*/
-
-}
+	
+    let inputName = document.getElementById("name");
+    let inputLastName = document.getElementById("last-name");
+    let inputEmail = document.getElementById("email");
+    let inputCountry = document.getElementById("country");
+    let inputPhone = document.getElementById("phone-number");
+    let inputJob = document.getElementById("job");
+    
+    
+    let registeredPerson = {
+        name   : inputName.value,
+        last : inputLastName.value,
+        email      : inputEmail.value,
+        country    : inputCountry.value,
+        phone : inputPhone.value,
+        job : inputJob.value
+      };
+        
+    let guests = JSON.parse(localStorage.getItem("Guests"));
+    if (guests == null)
+        guests = new Array(registeredPerson);
+    else
+        guests.push(registeredPerson);			
+    
+    let guests_serialized = JSON.stringify(guests);	
+    localStorage.setItem("Guests", guests_serialized); 	
+    
+    }
